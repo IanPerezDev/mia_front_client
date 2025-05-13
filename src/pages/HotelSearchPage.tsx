a90ca0c20b3dfde01b82c47410c56b14011af94c
juanincho <sabbaghjuan7@gmail.com>
2025-05-13 05:52:51 -0600

    busqueda de hoteles en manual corregida

--- src/pages/HotelSearchPage.tsx
+++ src/pages/HotelSearchPage.tsx
@@ -100,11 +100,13 @@ export const HotelSearchPage: React.FC<HotelSearchPageProps> = ({ onBack }) => {
 
     // Search term filter
     if (searchTerm) {
+      
       const searchLower = searchTerm.toLowerCase();
+      console.log(searchLower)
       filtered = filtered.filter(hotel =>
-        hotel.hotel.toLowerCase().includes(searchLower) ||
-        hotel.ciudad.toLowerCase().includes(searchLower) ||
-        hotel.estado.toLowerCase().includes(searchLower)
+        hotel?.hotel?.toLowerCase().includes(searchLower) ||
+        hotel?.ciudad?.toLowerCase().includes(searchLower) ||
+        hotel?.estado?.toLowerCase().includes(searchLower)
       );
     }
 
